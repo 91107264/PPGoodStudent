2a38498b0bb0de76dcf00840330840c149f79393
Broden Black <91107264@mvusd.net>
2022-12-02 12:37:04 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        Student Alan = new Student("Alan", "Turing", 191254, 3.56f, true);
+        Student Florance = new Student("Florance", "Nightingale", 182010, 3.89f, false);
+        Student Grace = new Student("Grace", "Hopper", 190692, 3.99f, true);
+        System.out.println(Alan.toString());
+        System.out.println();
+        Florance.setID(95366);
+        Florance.setGPA(3.92f);
+        System.out.println("Florance's ID: " + Florance.getID());
+        System.out.println("Florance's GPA: " + Florance.getGPA());
+        System.out.println("Graces's first name: " + Grace.getFirstName());
+        System.out.println("Grace is a cs student: " + Grace.getCSStudent());
+        System.out.println(Alan.createID());
+        System.out.println(Florance.createID());
+        System.out.println(Grace.createID());
+        System.out.println(Alan.CSGPA());
+        System.out.println(Florance.CSGPA());
+        System.out.println(Grace.CSGPA());
+    }
+
+}
---
+++ src/Student.java
@@ -0,0 +1,78 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int id;
+    private float gpa;
+    private boolean csStudent;
+
+    public Student(String theFirstName, String theLastName, int Theid, float Thegpa, boolean iscsstudent) {
+        firstName = theFirstName;
+        lastName = theLastName;
+        id = Theid;
+        gpa = Thegpa;
+        csStudent = iscsstudent;
+    }
+
+    public void setFirstName(String name) {
+        firstName = name;
+    }
+    public void setLastName(String name) {
+        lastName = name;
+    }
+    public void setID(int id1) {
+        id = id1;
+    }
+    public void setGPA(float thegpa) {
+        gpa = thegpa;
+    }
+    public void setCSStudent(boolean isStudent) {
+        csStudent = isStudent;
+    }
+
+    public Student() {
+        firstName = "first name goes here";
+        lastName = "last name goes here";
+        id = 00000000;
+        gpa = 0;
+        csStudent = false;
+    }
+    public String createID() {
+        return firstName.substring(0,1) + lastName + (id / 1000);
+    }
+
+    public Float CSGPA() {
+        if(csStudent==true) {
+            return (gpa * 1.15f);
+        }
+        return gpa;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getID() {
+        return id;
+    }
+
+    public float getGPA() {
+        return gpa;
+    }
+
+    public boolean getCSStudent() {
+        return csStudent;
+    }
+
+    public String toString() {
+        String output = "First Name: " + firstName +
+                "\nLast Name: " + lastName +
+                "\nID number: " + id +
+                "\nGPA: " + gpa +
+                "\nThe Student is in computer science: " + csStudent;
+        return output;
+    }
+}
